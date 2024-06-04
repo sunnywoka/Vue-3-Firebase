67784daea6c715ad570b3c3cfa6c0fdb1161e249
Yuekai Wu <yuekaixx@outlook.com>
2024-06-04 23:06:25 +1200

    finish lesson-15 task

--- app.js
+++ app.js
@@ -3,17 +3,37 @@ const app = Vue.createApp({
     return {
       showBooks: true,
       books: [
-        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
-        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
-        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
+        {
+          title: 'name of the wind',
+          author: 'patrick rothfuss',
+          img: 'assets/1.jpg',
+          isFav: true,
+        },
+        {
+          title: 'the way of kings',
+          author: 'brandon sanderson',
+          img: 'assets/2.jpg',
+          isFav: false,
+        },
+        {
+          title: 'the final empire',
+          author: 'brandon sanderson',
+          img: 'assets/3.jpg',
+          isFav: true,
+        },
       ],
     }
   },
   methods: {
     toggleShowBooks() {
-      this.showBooks =  !this.showBooks
+      this.showBooks = !this.showBooks
     },
-  }
+
+    toggleFavBook(book) {
+      book.isFav = !book.isFav
+      //console.log('check')
+    },
+  },
 })
 
 app.mount('#app')
